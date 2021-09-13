af2aee315974fbd647d63f8354e666b6303b824d
ankit-sr <ankitrajput6622@gmail.com>
2021-09-13 19:50:41 +0530

    completed modal work

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 
 // Confirm Password Validation for Register page
-var check = function() {
+const check = function() {
     if (document.getElementById('password').value ==
       document.getElementById('cpassword').value) {
       document.getElementById('message').style.color = 'green';
@@ -10,3 +10,24 @@ var check = function() {
       document.getElementById('message').innerHTML = 'Password and confirm password should be same!';
     }
   }
+
+
+const passIdtoModal = function(id, name, desc, price, imgid) {
+
+  document.getElementById('productModalLabel').innerText = name;
+  document.getElementById('prodDesc').innerText = desc;
+  document.getElementById('prodPrice').innerText = price;
+
+  // Selecting modal form
+  form = document.getElementById('modalform');
+  form.setAttribute('action', `/add_to_cart/${id}`);
+
+  // Selecting img tag in modal
+  img = document.getElementById('modalimg');
+  img.setAttribute('src', `/getImg/${imgid}`);
+  console.log(imgid);
+
+  console.log(img);
+}
+
+
